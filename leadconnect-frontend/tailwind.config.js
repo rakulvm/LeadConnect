840b990c61e7d2e30bedbc826dac4d1d3217afff
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-05-30 17:28:45 -0400

    setup intial including tailwind

--- leadconnect-frontend/tailwind.config.js
+++ leadconnect-frontend/tailwind.config.js
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      backgroundColor: '#F6F6F6',
-      cardWhite: '#FFFFFF',
-      highlightBlue: '#F0F4FF',
-      buttonBlue: '#4D78FF',
-      searchBarBackground: "#F5F6FA"
-    },
     extend: {},
   },
   plugins: [],
 }
+
